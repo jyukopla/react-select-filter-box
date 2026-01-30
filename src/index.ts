a75d672bb08099cc8093cd2f927223e89e798ebc
Asko Soukka <asko.soukka@iki.fi>
2026-01-30 18:37:04 +0000

    feat: add main package exports and update TODO progress
    - Create src/index.ts with all public exports
    - Export main FilterBox component
    - Export types, utilities, and hooks
    - Export core StateMachine for advanced use cases

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+/**
+ * react-select-filter-box
+ *
+ * A Sequential Filter Box component for React that provides a Select2-like
+ * single-line filter expression builder.
+ */
+
+// Main Component
+export { FilterBox, type FilterBoxProps } from './components'
+
+// Individual Components
+export {
+  Token,
+  TokenInput,
+  TokenContainer,
+  AutocompleteDropdown,
+  type TokenProps,
+  type TokenInputProps,
+  type TokenContainerProps,
+  type AutocompleteDropdownProps,
+} from './components'
+
+// Hooks
+export { useFilterState, type UseFilterStateProps, type UseFilterStateReturn } from './hooks'
+
+// Types
+export {
+  type FilterExpression,
+  type FilterCondition,
+  type FilterSchema,
+  type FieldConfig,
+  type OperatorConfig,
+  type FieldType,
+  type FieldValue,
+  type OperatorValue,
+  type ConditionValue,
+  type ConnectorValue,
+  type TokenData,
+  type AutocompleteItem,
+  type ValidationResult,
+  getDefaultOperators,
+  DEFAULT_CONNECTORS,
+  STRING_OPERATORS,
+  NUMBER_OPERATORS,
+  DATE_OPERATORS,
+  BOOLEAN_OPERATORS,
+  ENUM_OPERATORS,
+  ID_OPERATORS,
+} from './types'
+
+// Utilities
+export {
+  serialize,
+  deserialize,
+  toDisplayString,
+  toQueryString,
+  fromQueryString,
+  type SerializedExpression,
+} from './utils'
+
+// Core (for advanced use cases)
+export { FilterStateMachine, type FilterStep } from './core'
